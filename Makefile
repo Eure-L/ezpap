6311910d2d1c173f038154947c33599c2e7103ec
Vincent Jeunehomme <vjeunehomme@emi.u-bordeaux.fr>
2021-02-12 11:01:15 +0100

    fin tp5

--- Makefile
+++ Makefile
@@ -15,9 +15,6 @@ ENABLE_MPI			= 1
 
 ####################################
 
-#TP5
-CFLAGS                 += -O2 -Wall -Wno-unused-function -march=native
-
 ARCH		:= $(shell uname -s | tr a-z A-Z)
 
 ifdef ENABLE_SDL
--- kernel/c/rotation90.c
+++ kernel/c/rotation90.c
@@ -22,6 +22,75 @@ unsigned rotation90_compute_seq (unsigned nb_iter)
 
     swap_images ();
   }
+  return 0;
+}
 
+
+static void do_tile_reg (int x, int y, int width, int height)
+{
+  for (int i = y; i < y + height; i++)
+    for (int j = x; j < x + width; j++)
+      next_img (DIM-j-1, i) = cur_img (i, j);
+}
+
+static inline void do_tile (int x, int y, int width, int height, int who)
+{
+  monitoring_start_tile (who);
+
+  do_tile_reg (x, y, width, height);
+
+  monitoring_end_tile (x, y, width, height, who);
+}
+
+///////////////////////////// Simple sequential version (seq)
+// Suggested cmdline:
+// ./run --load-image images/shibuya.png -k rotation90 -v tiled -i 20 -ts 16
+//
+unsigned rotation90_compute_tiled (unsigned nb_iter)
+{
+   
+  for (unsigned it = 1; it <= nb_iter; it++) {
+
+    for (int y = 0; y < DIM; y += TILE_H)
+      for (int x = 0; x < DIM; x += TILE_W)
+        do_tile_reg (x, y, TILE_W, TILE_H);
+    swap_images ();
+  }
+  return 0;
+}
+
+///////////////////////////// Simple sequential version (seq)
+// Suggested cmdline:
+// ./run --load-image images/shibuya.png -k rotation90 -v omp_tiled -i 20 -ts 16 -n -t
+//
+unsigned rotation90_compute_omp_tiled (unsigned nb_iter)
+{
+  
+  for (unsigned it = 1; it <= nb_iter; it++) {
+    #pragma omp parallel for collapse(2) schedule(runtime) 
+    for (int y = 0; y < DIM; y += TILE_H)
+      for (int x = 0; x < DIM; x += TILE_W)
+        do_tile_reg (x, y, TILE_W, TILE_H);
+    swap_images ();
+  }
+  return 0;
+}
+
+///////////////////////////// Simple sequential version (seq)
+// Suggested cmdline:
+// ./run --load-image images/shibuya.png -k rotation90 -v omp_task -i 20 -ts 16 -n -t
+//
+unsigned rotation90_compute_omp_task (unsigned nb_iter)
+{
+  
+  for (unsigned it = 1; it <= nb_iter; it++) {
+    #pragma omp parallel 
+    #pragma omp single 
+    for (int y = 0; y < DIM; y += TILE_H)
+      for (int x = 0; x < DIM; x += TILE_W)
+        #pragma omp task
+        do_tile (x, y, TILE_W, TILE_H,omp_get_thread_num());
+    swap_images ();
+  }
   return 0;
 }
