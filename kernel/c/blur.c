85d8ee3d92892cccc2dc3e2a309efde1452cb0ff
Vincent Jeunehomme <vjeunehomme@emi.u-bordeaux.fr>
2021-03-05 11:01:08 +0100

    blur

--- kernel/c/blur.c
+++ kernel/c/blur.c
@@ -116,9 +116,25 @@ unsigned blur_compute_tiled (unsigned nb_iter)
   return 0;
 }
 
+unsigned blur_compute_tiled_omp (unsigned nb_iter)
+{
+  for (unsigned it = 1; it <= nb_iter; it++) {
+    #pragma omp parallel
+    #pragma omp for collapse(2) schedule (dynamic)
+    for (int y = 0; y < DIM; y += TILE_H)
+      for (int x = 0; x < DIM; x += TILE_W)
+        do_tile (x, y, TILE_W, TILE_H, omp_get_thread_num());
+        
+
+    swap_images ();
+  }
+
+  return 0;
+}
+
 ///////////////////////////// Tiled sequential opt version (tiled)
 // Suggested cmdline(s):
-// ./run -l images/1024.png -k blur -v tiled_opt -ts 32 -m si 
+// ./run -l images/1024.png -k blur -v tiled_opt -ts 32 -m
 //
 unsigned blur_compute_tiled_opt (unsigned nb_iter)
 {
@@ -138,5 +154,51 @@ unsigned blur_compute_tiled_opt (unsigned nb_iter)
     swap_images ();
   }
 
+  return 0;
+}
+
+///////////////////////////// Tiled sequential opt version (tiled)
+// Suggested cmdline(s):
+// ./run -l images/1024.png -k blur -v tiled_omp_opt -ts 128 -m
+//
+unsigned blur_compute_omp_tiled (unsigned nb_iter)
+{
+  for (unsigned it = 1; it <= nb_iter; it++) {
+    
+    //outer tiles
+    #pragma omp parallel
+    {  
+      #pragma omp for nowait schedule(dynamic)
+      for (int x = 0; x < DIM; x += TILE_W){//top  
+        do_tile (x, 0, TILE_W, 1, omp_get_thread_num());
+        do_inner_tile(x, 1, TILE_W, TILE_H-1, omp_get_thread_num());
+      }
+
+      //printf("test2 %d\n",it);
+      #pragma omp for nowait schedule(dynamic)
+      for (int x = 0; x < DIM; x += TILE_W){//bottom
+        do_tile (x, DIM-1, TILE_W, 1, omp_get_thread_num());
+        do_inner_tile(x, DIM-TILE_H-1, TILE_W, TILE_H-1, omp_get_thread_num());
+      }
+      #pragma omp for nowait schedule(dynamic)
+      for (int y = TILE_H; y < DIM-TILE_H; y += TILE_H){//left
+        do_tile (0, y, 1, TILE_H, omp_get_thread_num());
+        do_inner_tile(1, y, TILE_W-1, TILE_H, omp_get_thread_num());
+      }
+      #pragma omp for nowait schedule(dynamic)
+      for (int y = TILE_H; y < DIM-TILE_H; y += TILE_H){//right
+        do_tile (DIM-1, y, 1, TILE_H, omp_get_thread_num());
+        do_inner_tile (DIM-TILE_W, y, TILE_W-1, TILE_H, omp_get_thread_num());
+      } 
+      
+      #pragma omp for collapse(2) schedule (guided)
+      for (int y = TILE_H; y < DIM-TILE_H; y += TILE_H){
+        for (int x = TILE_W; x < DIM-TILE_W; x += TILE_W)
+          do_inner_tile(x, y, TILE_W, TILE_H, omp_get_thread_num());
+     
+      }
+    }
+  swap_images ();
+  }
   return 0;
 }
